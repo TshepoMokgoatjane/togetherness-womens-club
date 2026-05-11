62b4478a17d0d05d361d60d644b791451a0e94dd
F5103762 <Tshepo.Mokgoatjane@fnb.o.za>
2026-05-11 18:13:51 +0200

    Initial Spring Boot project scaffold

---
+++ src/main/java/za/co/twc/togetherness/womens/club/TogethernessWomensClubApplication.java
@@ -0,0 +1,13 @@
+package za.co.twc.togetherness.womens.club;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogethernessWomensClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogethernessWomensClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/twc/togetherness/womens/club/TogethernessWomensClubApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.twc.togetherness.womens.club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogethernessWomensClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
