994fd4fb8bd0d2a6ba4b288826499c4641a0b12a
F5103762 <Tshepo.Mokgoatjane@fnb.o.za>
2026-05-12 16:28:56 +0200

    Remove the no-arg constructor in MemberNotFoundException() class #9

--- src/main/java/za/co/twc/togetherness/womens/club/exception/MemberNotFoundException.java
+++ src/main/java/za/co/twc/togetherness/womens/club/exception/MemberNotFoundException.java
@@ -2,8 +2,6 @@ package za.co.twc.togetherness.womens.club.exception;
 
 public class MemberNotFoundException extends RuntimeException {
 
-    public MemberNotFoundException() {}
-
     public MemberNotFoundException(Long id) {
         super("Member with id " + id + " not found");
     }
