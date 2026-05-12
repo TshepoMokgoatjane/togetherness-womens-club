8c99a164920e7c1e03c4de82d5d64e0c2061eb6e
F5103762 <Tshepo.Mokgoatjane@fnb.o.za>
2026-05-12 14:43:26 +0200

    Issue 2: Add MemberRepository - #3

---
+++ src/main/java/za/co/twc/togetherness/womens/club/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package za.co.twc.togetherness.womens.club.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.co.twc.togetherness.womens.club.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByDeletedFalse();
+
+    Optional<Member> findByIdAndDeletedFalse(Long id);
+}
