5bddbf319f49d02212c6b6bfc840e1ca7ef6fda6
F5103762 <Tshepo.Mokgoatjane@fnb.o.za>
2026-05-19 10:45:10 +0200

    Revert cron job to run on the 1st day of each and every month and removed unused import statements. #46

--- src/main/java/za/co/twc/togetherness/womens/club/scheduler/ContributionScheduler.java
+++ src/main/java/za/co/twc/togetherness/womens/club/scheduler/ContributionScheduler.java
@@ -5,13 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import za.co.twc.togetherness.womens.club.domain.Contribution;
-import za.co.twc.togetherness.womens.club.domain.ContributionStatus;
-import za.co.twc.togetherness.womens.club.repository.ContributionRepository;
 import za.co.twc.togetherness.womens.club.service.ContributionService;
-
-import java.time.YearMonth;
-import java.util.List;
 
 @Component
 public class ContributionScheduler {
@@ -25,11 +19,9 @@ public class ContributionScheduler {
         this.contributionService = contributionService;
     }
 
+    // For TESTING PURPOSES - Change cron to run every minute: @Scheduled(cron = "0 */1 * * * ?")
     // Runs on the 1st day of every month at midnight
-    //@Scheduled(cron = "0 0 0 1 * ?")
-
-    // For TESTING PURPOSES - Change cron to run every minute:
-    @Scheduled(cron = "0 */1 * * * ?")
+    @Scheduled(cron = "0 0 0 1 * ?")
     public void generateMonthlyContributions() {
         LOGGER.info("Generating monthly contributions for this month.");
         contributionService.generateMonthlyContributions();
